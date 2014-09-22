03584d64f00f543dd0b3fd0fcea1abafecf65bfc
Merhawi Fissehaye <merhawifissehaye@gmail.com>
2014-09-22 18:09:40 +0300

    Initial release

---
+++ index.php
@@ -0,0 +1,67 @@
+<html>
+	<head>
+		<title>PCMS</title>
+		<link type="text/css" rel="stylesheet" href="static/css/bootstrap.min.css" />
+		<link type="text/css" rel="stylesheet" href="static/css/style.css" />
+		<link type="text/css" rel="stylesheet" href="static/fonts/font-awesome/css/font-awesome.min.css" />
+
+	</head>
+
+	<body>
+		<div class="navbar navbar-static-top navbar-custom">
+			<div class="navbar-inner navbar-fixed-top">
+				<button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse"></button>
+				<a href="#" class="brand">
+					<img src="static/img/PCMS/logo.png" />
+					<div class="brand-text">
+						<h3>Betek Construction <small>PLC</small></h3><br>
+						<p>Petty Cash Management System</p>
+					</div>
+				</a>
+				<div>
+					<ul class="nav navbar-nav navbar-right">
+						<li class="dropdown">
+							<a href="#" class="dropdown-toggle" data-toggle="dropdown">Biniam Teklebrhan<i class="fa fa-caret-down"></i><br><small>General Manager</small></a>
+							<ul class="dropdown-menu" role="menu">
+								<li><a href="#">Account Setting</a></li>
+								<li><a href="#">Logout</a></li>
+							</ul>
+						</li>
+						<li><a href="#" title="Settings"><i class="fa fa-cog"></i></a></li>
+					</ul>
+				</div>
+			</div>
+			<div class="sidebar-wrapper">
+				<ul class="sidebar-nav">
+					<li class="active"><a href="#"><i class="fa fa-home"></i>Home</a></li>
+					<li><a href="#"><i class="fa fa-inbox"></i>Messages<span class="alarm">2</span></a></li>
+					<li><a href="#"><i class="fa fa-dollar"></i>Transactions</a></li>
+					<li><a href="#"><i class="fa fa-user"></i>Users</a></li>
+					<li><a href="#"><i class="fa fa-book"></i>Reports</a></li>
+					<li><a href="#"><i class="fa fa-file"></i>Invoices</a></li>
+				</ul>
+			</div>
+		</div>
+	</div>
+	<div class="container content">
+		<div class="content-header">
+			<ul class="row">
+				<li class="col-md-5">Birr 800.00<br><small>Current Balance</small></li>
+				<li class="col-md-5">Birr 0.00<br><small>Withdrawn Yet</small></li>
+				<li class="col-md-5">2 Unread Messages</li>
+				<div class="clear"></div>
+			</ul>
+		</div>
+		<div class="jumbotron">
+			<h1>Welcome to Petty Cash Management System</h1>
+			<a href="#" class="btn btn-primary">Learn more about PCMS</a>
+		</div>
+	</div>
+	<footer>
+		<p class="left">2014 &copy; Betek Construction</p>
+		<p class="right">Designed &amp; Developed by Malam Engineering PLC</p>
+	</footer>
+	</body>
+	<script type="text/javascript" src="static/js/jquery-1.11.0.min.js"></script>
+	<script type="text/javascript" src="static/js/bootstrap.min.js"></script>
+</html>
